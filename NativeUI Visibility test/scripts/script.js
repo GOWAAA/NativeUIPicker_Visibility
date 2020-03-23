73a82495d3f3e407ed73ecddb3738b8a95f162c2
Boon Jun <aw.boon.jun@gmail.com>
2020-03-23 16:47:38 +0800

    new file:   Instagram_Filters_UI_1.jpg 	new file:   NativeUI Visibility test/NativeUI Visibility test.arproj 	new file:   NativeUI Visibility test/scripts/script.js 	new file:   NativeUI Visibility test/scripts/tsconfig.json 	new file:   NativeUI Visibility test/textures/Blue.png 	new file:   NativeUI Visibility test/textures/Green.png 	new file:   NativeUI Visibility test/textures/Red.png 	new file:   NativeUI Visibility test/textures/Yellow.png 	new file:   readme.md

---
+++ NativeUI Visibility test/scripts/script.js
@@ -0,0 +1,84 @@
+/**
+ * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ */
+
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//==============================================================================
+const Scene = require('Scene'); 
+const NativeUI = require('NativeUI'); 
+const Textures = require('Textures'); 
+
+//Declare an empty list
+const object = [];
+//Enter no. of object you wish to toggle visibility with native UI picker
+const object_no = 4
+
+// Find your objects 
+//make sure you name your object in order back in Spark AR starting from 0
+//e.g Object0, Object1, Object2 etc...
+for(let i = 0; i<object_no; i++){
+    //Add all objects you want into the empty list you created above
+    //Only objects you name in the format "object0" will be added, so make sure you don't have other object name this way in SparkAR to
+    //avoid adding the wrong things here. Also, we count starting from 0 here!
+    object.push(Scene.root.find('object'+i)); 
+}
+
+
+//Set default visibility when effect launch
+//Note that when hidden is false, object will be visible
+for(let i = 0; i<object_no; i++){ 
+    if (i == 0){ //The first object, object0 will be visible by default. 
+        object[i].hidden = false;
+    }
+    else{
+        object[i].hidden = true;    
+    }
+}
+
+//Set index = 0
+const index = 0;
+
+// Create a configuration object 
+const configuration = { 
+// The index of the selected item in the picker 
+selectedIndex: index, 
+// The image textures (in sequence) to use as the items in the picker 
+// Make sure these textures are set to uncompressed or this *will not work* 
+items: [ 
+{image_texture: Textures.get('Green')}, 
+{image_texture: Textures.get('Blue')},
+{image_texture: Textures.get('Red')},
+{image_texture: Textures.get('Yellow')}
+ ], 
+}
+
+// Create our picker 
+const picker = NativeUI.picker; 
+
+// Load the configuration 
+picker.configure(configuration); 
+
+// Set the visibility to true 
+picker.visible = true; 
+
+// When the user selects an item form the picker, a new val value will be passed into the loop below 
+picker.selectedIndex.monitor().subscribe(function(val) { 
+    for(let i = 0; i<object_no; i++){
+        if (i == val.newValue){ 
+            //val.newvalue will be 0 when 1st picker is chosen, and 1 when 2nd picker is chosen so on and so forth
+            //hence, every time a new picker option is chosen, this for loop will check which picker is chosen and set the corresponding
+            //object to be visible
+            object[i].hidden = false;
+        }
+        else{
+            object[i].hidden = true;    
+        }
+    }
+}
+);
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Using Native UI Picker in Spark AR to toggle visibility of object For Instagram Filters
+
+This project file demonstrate how you can toggle visibility of objects using native UI pickers for your Instagram filters. 
+
+![Instagram Filter](./Instagram_Filters_UI_1.jpg)
+
+You can also go to the 
+
+[tutorial]: https://www.gowaaa.com/post/using-native-ui-picker-in-spark-ar-to-toggle-visibility-of-object-for-instagram-filters
+
+ to find out more on how you can use this project file. 
